9765dde3e6fdd172f729ed5aad6e8697d6c7057b
zaeberg <andrey.berg.dev@gmail.com>
2024-03-16 22:41:46 +0200

    refactor: nvim update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,11 +3,15 @@ return {
     "folke/tokyonight.nvim",
     opts = { style = "moon" },
   },
-
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "catppuccin",
     },
   },
 }
